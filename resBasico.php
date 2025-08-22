77b0dc84dc36160d82bb2f50c62fae612e80985e
ProyectoSUGAR <proyectoithink5@gmail.com>
2025-08-22 15:50:03 -0300

    Update resBasico.php

---
+++ resBasico.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+    $op = $_POST["operacion"];
+    $resultado = 0;
+
+    switch ($op) {
+        case "+":
+            $resultado = $num1 + $num2;
+            break;
+        case "-":
+            $resultado = $num1 - $num2;
+            break;
+        case "*":
+            $resultado = $num1 * $num2;
+            break;
+        case "/":
+            if ($num2 != 0) {
+                $resultado = $num1 / $num2;
+            } else {
+                $resultado = "Error: División por cero";
+            }
+            break;
+        default:
+            $resultado = "Operación no válida";
+            break;
+    }
+    echo "<h1>Resultado: $resultado</h1>";
+}
+?>
